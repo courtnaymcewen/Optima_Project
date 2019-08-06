53e5feeb30b662a6fc52d16a68c5a48948efaae6
mpatrick1024 <mpatrick1024@gmail.com>
2019-08-06 15:29:28 -0500

    results update

--- apps/optima_app/views.py
+++ apps/optima_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, "optima_app/index.html")
+
+def results (request):
+    return render (request, "optima_app/results.html")
