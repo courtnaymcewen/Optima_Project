a5d4607808576c0fcb15305b48c2a6b534da19a5
Carla Miles <ms.carla.albert@gmail.com>
2019-08-07 16:22:02 -0500

    worked on making all pages cohesive

--- apps/optima_app/views.py
+++ apps/optima_app/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from apps.login_app.models import User
 
 def index(request):
-    return render(request, "optima_app/index.html")
+    print('*'*50, 'showing the home page')
+    if 'new_user_id' not in request.session:
+        return render(request, "optima_app/index.html")
+    else:
+        user = User.objects.get(id=request.session['new_user_id'])
+        context = {
+            'user': user
+        }
+        return render(request, "optima_app/index.html", context)
 
 def results (request):
-    return render (request, "optima_app/results.html")
+    print('*'*50, 'showing the results page')
+    return render(request, "optima_app/results.html")
+
+def show_account(request, user_id):
+    print('*'*50, 'showing the update account page')
+    if 'new_user_id' not in request.session:
+        return redirect('/login')
+    else:
+        user = User.objects.get(id=user_id)
+        context = {
+            'user': user
+        }
+        return render(request, "optima_app/update_account.html", context)
